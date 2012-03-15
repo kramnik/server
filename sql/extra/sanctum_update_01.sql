c4729397a6037ef8c93dbb7fad9020f4893c4033
Xfurry <xfurry@scriptdev2.com>
2012-03-16 01:41:51 +0200

    Update for quest 10162

--- sql/extra/sanctum_update_01.sql
+++ sql/extra/sanctum_update_01.sql
@@ -1,5 +1,10 @@
 -- Sanctum updates
 
--- Eye of Kilrogg
+-- Eye of Kilrogg aura
 DELETE FROM `creature_template_addon` WHERE (`entry`=4277);
 INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `emote`, `moveflags`, `auras`) VALUES (4277, 0, 0, 1, 0, 0, 2585);
+
+-- quest 10162 spell target area
+delete from spell_area where spell=33836;
+insert into spell_area values
+(33836,3803,0,0,0,0,0,2,0);
